3530890510f2c7d5d85239c9b89104bb7d55e5bc
SuperConsole <a9hfb@svk.jp>
2019-07-01 09:27:11 +0900

    add case:hasSpare

--- bowling_game/bowling_game.py
+++ bowling_game/bowling_game.py
@@ -1,8 +1,19 @@
 class BowlingGame:
-    score=0
+    isLastFrame = False
+    isSpare     = False
+    lastScore   = 0
+    score       = 0
 
     def get_total_score(self):
         return self.score
     
-    def record_shot(self, point):
-        self.score += point
+    def record_shot(self, pins):
+        self.score += pins
+        if self.isSpare:
+            self.score += pins
+            self.isSpare =   False
+        if self.isLastFrame and self.lastScore + pins   ==  10:
+            self.isSpare = True
+        else:
+            self.lastScore  = pins
+        self.isLastFrame = not(self.isLastFrame)
--- bowling_game/bowling_game_test.py
+++ bowling_game/bowling_game_test.py
@@ -2,24 +2,45 @@ import unittest
 import bowling_game
 
 class BowlingGameTest(unittest.TestCase):
-    game=None
+    game    = None
 
     def setUp(self):
         self.game = bowling_game.BowlingGame()
 
     def test_all_gutter_game(self):
+        #ALL GUTTER
+        #score: 0
         self.record_n_shot(0, 20)
-        
         self.assertEqual(0, self.game.get_total_score())
 
     def test_all_one_point_game(self):
+        #ALL ONE PIN
+        #score:20
         self.record_n_shot(1, 20)
-        
         self.assertEqual(20, self.game.get_total_score())
+
+    def test_has_one_spare_game(self):
+        #ONE SPARE
+        #score: 18
+        self.game.record_shot(3)
+        self.game.record_shot(7)
+        self.game.record_shot(4)
+        self.record_n_shot(0, 17)
+        self.assertEqual(18, self.game.get_total_score())
+
+    def test_has_no_spare_game_2(self):
+        #NO SPARE
+        #score: 13
+        self.game.record_shot(2)
+        self.game.record_shot(5)
+        self.game.record_shot(5)
+        self.game.record_shot(1)
+        self.record_n_shot(0, 16)
+        self.assertEqual(13, self.game.get_total_score())
 
     def record_n_shot(self, point, n):
         for i in range(1,n+1):
             self.game.record_shot(point)
 
-if __name__ == "__main__":
+if __name__ ==  "__main__":
     unittest.main()
